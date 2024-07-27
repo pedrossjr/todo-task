8dee5664e46f69ee2851600266351c8d746c3cf6
pedrossjr <pedrossjr@yahoo.com.br>
2024-07-26 23:56:14 -0300

    Nova atualização

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,87 +1,132 @@
 const Task = require('../models/Task')
 
+let message = null;
+let type = null;
+
 module.exports = class TaskController {
-    //Chama o formulário para adicionar tarefa
+    // EXIBE O FORMULÁRIO DE CRIAÇÃO DE UMA TAREFA
     static createTask(req, res) {
-        res.render('tasks/create')
+        res.render('tasks/create');
     }
 
-    //Executa a inclusão da tarefa
+    // EXECUTA A INCLUSÃO DE UMA TAREFA
     static async createTaskSave(req, res) {
-        try {
-            const task = {
-                title: req.body.title,
-                description: req.body.description,
-                done: false
-            }
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
 
+        if (!task.title) {
+            message = "O campo título deve ser informado!";
+            type = 'danger';
+            return res.render('tasks/create', { message, type });
+        }
+
+        if (!task.description) {
+            message = "O campo descrição deve ser informado!";
+            type = 'danger';
+            return res.render('tasks/create', { message, type });
+        }
+
+        try {
             await Task.create(task)
+            message = "Tarefa criada com sucesso!";
+            type = 'success';
 
-            res.redirect('/tasks')
+            res.render('tasks/create', { message, type, redirect: true });
         } catch (e) {
-            return console.log(e);
+            return res.status(500).send({ error: e.message });
         }
     }
 
-    //Remove uma tarefa
+    // EXECUTA A EXCLUSÃO DE UMA TAREFA
     static async removeTask(req, res) {
+        const id = req.body.id
+
         try {
-            const id = req.body.id
-
             await Task.destroy({ where: { id: id } })
-
+            message = 'Tarefa excluída com sucesso!';
+            type = 'success';
             res.redirect('/tasks')
         } catch (e) {
-            return console.log(e);
+            return res.status(500).send({ error: e.message });
         }
     }
 
-    //Editar tarefa
+    // EXIBE O FORMULÁRIO DE EDIÇÃO DE UMA TAREFA
     static async updateTask(req, res) {
         const id = req.params.id
 
         const task = await Task.findOne({ where: { id: id }, raw: true })
 
-        res.render('tasks/edit', { task })
+        res.render('tasks/edit', { task });
     }
 
+    // EXECUTA A ATUALIZAÇÃO DE UMA TAREFA
     static async updateTaskPost(req, res) {
-        try {
-            const id = req.body.id
+        const GetId = req.body.id;
 
-            const task = {
-                title: req.body.title,
-                description: req.body.description
+        const task = {
+            id: req.body.id,
+            title: req.body.title,
+            description: req.body.description
+        }
+
+        try {
+            if (!task.title) {
+                message = "O campo título deve ser informado!";
+                type = 'danger';
+
+                return res.render('tasks/edit', { message, type });
             }
 
-            await Task.update(task, { where: { id: id } })
+            if (!task.description) {
+                message = "O campo descrição deve ser informado!";
+                type = 'danger';
+                return res.render('tasks/edit', { message, type });
+            }
 
-            res.redirect('/tasks')
+            await Task.update(task, { where: { id: GetId } })
+
+            message = "Tarefa atualizada com sucesso!";
+            type = 'success';
+
+            res.render('tasks/edit', { message, type, redirect: true });
         } catch (e) {
-            return console.log(e);
+            return res.status(500).send({ error: e.message });
         }
     }
 
+    // EXECUTA A ATUALIZAÇÃO DE UMA TAREFA PARA CONCLUÍDA OU NÃO CONCLUÍDA
     static async toggleTaskStatus(req, res) {
+        const id = req.body.id
+
+        const task = {
+            done: req.body.done === '0' ? true : false
+        }
+
         try {
-            const id = req.body.id
+            await Task.update(task, { where: { id: id } });
 
-            const task = {
-                done: req.body.done === '0' ? true : false
-            }
-
-            await Task.update(task, { where: { id: id } })
-
-            res.redirect('/tasks')
+            res.redirect('/tasks');
         } catch (e) {
-            return console.log(e);
+            return res.status(500).send({ error: e.message });
         }
     }
 
-    //Lista todas as tarefas
+    // EXIBE A LISTA DE TAREFAS
     static async showTasks(req, res) {
-        const tasks = await Task.findAll({ raw: true })
+        try {
+            setTimeout(() => {
+                message = ''
+            }, 2000)
 
-        res.render('tasks/all', { tasks })
+            const tasks = await Task.findAll({ raw: true })
+
+            res.render('tasks/all', { tasks })
+        } catch (e) {
+            return res.status(500).send({ error: e.message });
+        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+
 const exphbs = require('express-handlebars')
+
 const app = express()
 
 const conn = require('../db/conn')
